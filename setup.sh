24dac7e0457335ab2049cb19bab99144295735b8
howpwn <finn79426@gmail.comm>
2018-05-27 16:52:57 +0800

    :heavy_plus_sign: Setup.sh add

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# setup.sh
+# Copyright (C) 2018 howpwn <finn79426@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+sudo -H pip install pyperclip
+sudo -H pip install termcolor
+
